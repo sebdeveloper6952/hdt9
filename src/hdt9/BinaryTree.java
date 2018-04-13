893ccbb326647f9145bcc60ba7979b841eeb3700
sevic69 <sebdeveloper6952@gmail.com>
2018-04-13 11:02:30 -0600

    Clases BinaryTree, BinarySearchTree, y SplayTree

---
+++ src/hdt9/BinaryTree.java
@@ -0,0 +1,134 @@
+package hdt9;
+
+/**
+ *
+ * @author sebdeveloper6952
+ */
+public class BinaryTree<E>
+{
+    protected E value;
+    protected BinaryTree<E> parent, left, right;
+    
+    public BinaryTree()
+    {
+        value = null;
+        parent = left = right = null;
+    }
+    
+    public BinaryTree(E value)
+    {
+        this.value = value;
+        setLeft(new BinaryTree<>());
+        setRight(new BinaryTree<>());
+    }
+    
+    public BinaryTree(E value, BinaryTree<E> left, BinaryTree<E> right)
+    {
+        this(value);
+        if(left != null) setLeft(left); 
+        if(right != null) setRight(right);
+    }
+    
+    public BinaryTree<E> left() { return left; }
+    public BinaryTree<E> right() { return right; }
+    public BinaryTree<E> parent() { return parent; }
+
+    public void setLeft(BinaryTree<E> newLeft) 
+    {
+        left = newLeft;
+        newLeft.setParent(this);
+    }
+
+    public void setRight(BinaryTree<E> newRight)
+    {
+        right = newRight;
+        newRight.setParent(this);
+    }
+    
+    public boolean isLeftChild()
+    {
+        if(parent != null)
+        {
+            BinaryTree left = parent.left();
+            return this == left;
+        }
+        return false;
+    }
+    
+    public boolean isRightChild() 
+    {
+        if(parent != null)
+        {
+            BinaryTree right = parent.right();
+            return this == right;
+        }
+        return false;
+    }
+    
+    /**
+     * Return true if this node has at least one child, false otherwise.
+     */
+    public boolean isInternal()
+    {
+        return ((left() != null) || (right != null));
+    }
+
+    public E value() { return value; }
+    
+    public void setValue(E newValue) { value = newValue; }
+    
+    protected void setParent(BinaryTree<E> newParent) 
+    {
+        parent = newParent;
+    }
+
+    protected boolean isEmpty()
+    {
+        return value == null;
+    }
+    
+    /**
+     * Rotates right over this node.
+     * pre: this node has a left child.
+     * post: This node is one level lower. Left child is one level higher.
+     * @return The new root.
+     */
+    protected BinaryTree<E> rotateRight()
+    {
+        BinaryTree<E> newRoot = left();
+        if(newRoot == null) return this;
+        BinaryTree<E> tempParent = this.parent();
+        boolean isLeft = this.isLeftChild();
+        setLeft(newRoot.right());
+        newRoot.setRight(this);
+        if(tempParent != null)
+        {
+            if(isLeft) tempParent.setLeft(newRoot);
+            else tempParent.setRight(newRoot);
+        }
+        else newRoot.setParent(null);
+        return newRoot;
+    }
+    /**
+     * Rotates left over this node.
+     * pre: this node has a right child.
+     * post: This node is one level lower. Right child is one level higher.
+     * @return The new root.
+     */
+    protected BinaryTree<E> rotateLeft()
+    {
+        BinaryTree<E> newRoot = this.right();
+        if(newRoot == null) return this;
+        BinaryTree<E> tempParent = this.parent();
+        boolean isLeft = this.isLeftChild();
+        setRight(newRoot.left());
+        newRoot.setLeft(this);
+        if(tempParent != null)
+        {
+            if(isLeft) tempParent.setLeft(newRoot);
+            else tempParent.setRight(newRoot);
+        }
+        else newRoot.setParent(null);
+        return newRoot;
+    }
+}
---
+++ src/hdt9/SplayTree.java
@@ -0,0 +1,101 @@
+package hdt9;
+
+import java.util.Map.Entry;
+
+/**
+ *
+ * @author sebdeveloper6952
+ */
+public class SplayTree<K extends Comparable<K>, V> extends BinarySearchTree<K,V>
+{
+    public V put(K key, V value)
+    {
+        V old = super.put(key, value);
+        splay(actionNode);
+        return old;
+    }
+    
+    public V remove(K key)
+    {
+        V old = super.remove(key);
+        if(actionNode != null) splay(actionNode);
+        return old;
+    }
+    
+    public boolean contains(K key)
+    {
+        boolean b = super.contains(key);
+        if(b) splay(actionNode);
+        return b;
+    }
+    
+    public V get(K key)
+    {
+        V v = super.get(key);
+        if(v != null) splay(actionNode);
+        return v;
+    }
+    
+    /**
+     * Performs a sequence of rotations until the provided node is the root of
+     * the tree.
+     * @param node Node to splay.
+     * pre: node is not the root of the tree.
+     * post: node is the root of the tree.
+     */
+    protected void splay(BinaryTree<Entry<K,V>> node)
+    {
+        if(node.equals(root) || node == null) return;
+        // get the parent and grandparent of this node
+        BinaryTree<Entry<K,V>> gp = null;
+        BinaryTree<Entry<K,V>> p = null;
+        boolean hasGp = false;
+        while(node.parent() != null)
+        {
+            p = node.parent();
+            if(p != null) gp = p.parent();
+            hasGp = gp != null;
+            // <editor-fold defaultstate="collapsed" desc="Node is left child">
+            if (node.isLeftChild()) {
+                if (hasGp) // node has a grandparent
+                {
+                    if (p.isLeftChild()) // left-left
+                    {
+                        gp.rotateRight();
+                        p.rotateRight();
+                    } else // left-right
+                    {
+                        p.rotateRight();
+                        gp.rotateLeft();
+                    }
+                } else // left; node has only parent.
+                {                    
+                    p.rotateRight();
+                }
+            } // </editor-fold>
+            // <editor-fold defaultstate="collapsed" desc="Node is right child">
+            else // node is right child
+            {
+                if (hasGp) // node has a grandparent
+                {
+                    if (p.isLeftChild()) // right-left
+                    {
+                        p.rotateLeft();
+                        gp.rotateRight();
+                    } else // right-right
+                    {
+                        gp.rotateLeft();
+                        p.rotateLeft();
+                    }
+                } else // right; node has only parent.
+                {                    
+                    p.rotateLeft();
+                }
+            }
+
+// </editor-fold>
+        }
+        // the splayed node is now the root of the tree
+        root = node;
+    }
+}
